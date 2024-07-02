70a1722f6c7403784945fe49e4a1e030a410b315
Abraamyan Alexander <samvelcc25@outlook.com>
2024-07-02 19:05:59 +0300

    reduce with init value added

--- include/public/s0_parallel_algorithms_openmp.hpp
+++ include/public/s0_parallel_algorithms_openmp.hpp
@@ -12,25 +12,30 @@ namespace s0m4b0dY
 {
   class OpenMPI
   {
+    template < class T >
+    using IteratorValueType = _helpers::IteratorValueType<T>;
   public:
     template <_helpers::AddableIterator Iterator_t>
-    _helpers::IteratorValueType<Iterator_t>::value_type reduce(Iterator_t begin, Iterator_t end);
+    IteratorValueType<Iterator_t>::value_type reduce(Iterator_t begin, Iterator_t end);
+
+    template <_helpers::AddableIterator Iterator_t>
+    IteratorValueType<Iterator_t>::value_type reduce(Iterator_t begin, Iterator_t end, IteratorValueType<Iterator_t>::value_type initValue);
   };
 
   template <_helpers::AddableIterator Iterator_t>
   inline _helpers::IteratorValueType<Iterator_t>::value_type OpenMPI::reduce(Iterator_t begin, Iterator_t end)
   {
     using value_type = _helpers::IteratorValueType<Iterator_t>::value_type;
-    const value_type init_value = 0;
+    const value_type initValue = 0;
     std::vector<std::pair<Iterator_t, Iterator_t>> ranges = generateRanges(begin, end, omp_get_max_threads());
-    std::vector<value_type> results(ranges.size(), init_value);
+    std::vector<value_type> results(ranges.size(), initValue);
     try
     {
       #pragma omp parallel for
       for (auto i = 0; i < ranges.size(); ++i)
       {
         const auto &range = ranges[i];
-        value_type result = init_value;
+        value_type result = initValue;
         for (auto it = range.first; it != range.second; it++)
         {
           result += *it;
@@ -42,10 +47,42 @@ namespace s0m4b0dY
     {
       throw;
     }
-    auto result = init_value;
-    for (const auto &local_result : results)
+    auto result = initValue;
+    for (const auto &localResult : results)
     {
-      result += local_result;
+      result += localResult;
+    }
+    return result;
+  }
+
+  template<_helpers::AddableIterator Iterator_t>
+  inline _helpers::IteratorValueType<Iterator_t>::value_type OpenMPI::reduce(Iterator_t begin, Iterator_t end, IteratorValueType<Iterator_t>::value_type initValue)
+  {
+    using value_type = _helpers::IteratorValueType<Iterator_t>::value_type;
+    std::vector<std::pair<Iterator_t, Iterator_t>> ranges = generateRanges(begin, end, omp_get_max_threads());
+    std::vector<value_type> results(ranges.size(), initValue);
+    try
+    {
+      #pragma omp parallel for
+      for (auto i = 0; i < ranges.size(); ++i)
+      {
+        const auto &range = ranges[i];
+        value_type result = 0;
+        for (auto it = range.first; it != range.second; it++)
+        {
+          result += *it;
+        }
+        results[i] = result;
+      }
+    }
+    catch(const std::exception &e)
+    {
+      throw;
+    }
+    auto result = initValue;
+    for (const auto &localResult : results)
+    {
+      result += localResult;
     }
     return result;
   }
